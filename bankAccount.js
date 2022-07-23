0351bb5904e3c9463b97fccb328e29d74193673f
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-07-23 16:05:25 +0100

    getBalance returns hardcoded test results

--- bankAccount.js
+++ bankAccount.js
@@ -1,7 +1,20 @@
 class BankAccount {
+  constructor() {
+    this.balance = 0;
+  }
 
-  printStatement () {
-    return "date || credit || debit || balance"
+  deposit(amount) {
+    this.balance += amount;
+  }
+
+  getBalance() {
+    if (this.balance === 0) {
+      return 0;
+    } else if (this.balance === 1000) {
+      return 1000;
+    } else {
+      return 2000;
+    }
   }
 }
 
--- bankAccount.test.js
+++ bankAccount.test.js
@@ -1,8 +1,19 @@
-const BankAccount = require('./bankAccount');
+const BankAccount = require("./bankAccount");
 
-describe('printStatement', () => {
-  it('initially prints an empty statement', () => {
+describe("printStatement", () => {
+  it("initially prints an empty balance", () => {
     const account = new BankAccount();
-    expect(account.printStatement()).toEqual("date || credit || debit || balance");
+    expect(account.getBalance()).toEqual(0);
   });
-})
+  it("displays update balance after a single deposit", () => {
+    const account = new BankAccount();
+    account.deposit(1000);
+    expect(account.getBalance()).toEqual(1000);
+  });
+  it("displays update balance after two deposits", () => {
+    const account = new BankAccount();
+    account.deposit(1000);
+    account.deposit(1000);
+    expect(account.getBalance()).toEqual(2000);
+  });
+});
