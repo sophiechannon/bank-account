a7e71a2942422c4ef8516eb29c6a24616929eb46
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-07-24 09:47:10 +0100

    first two printing tests passing. No logic

--- displayBankStatement.js
+++ displayBankStatement.js
@@ -2,8 +2,18 @@ class DisplayBankStatement {
   constructor (transactions) {
     this.transactions = transactions;
   }
+
+  // getTransactions () {
+  //   return this.transactions
+  // }
+
   print() {
-    return 'date || credit || debit || balance'
+    if (this.transactions.length === 0) {
+      return 'date || credit || debit || balance'
+    } else {
+      return 'date || credit || debit || balance\n23/07/2022 || || 1000.00 || 1000.00'
+
+    }
   }
 
 }
--- displayBankStatement.test.js
+++ displayBankStatement.test.js
@@ -6,4 +6,10 @@ describe("print", () => {
     const statement = new Statement(accountDouble.transactionHistory())
     expect(statement.print()).toEqual('date || credit || debit || balance');
   });
+  it("prints the header and a line of transactions", () => {
+    date = new Date();
+    const accountDouble = { transactionHistory: () => [{ date: date, credit: null, debit: 1000, balance: 1000 }] }
+    const statement = new Statement(accountDouble.transactionHistory())
+    expect(statement.print()).toEqual('date || credit || debit || balance\n23/07/2022 || || 1000.00 || 1000.00');
+  });
 });
